514e4c1439f4bb192180d538f75a46004fa2b86e
Arturo Albacete <aalbacetef@gmail.com>
2024-03-07 00:07:44 +0100

    dumprom:  - split out code into separate files (dump, text)  - moved some of the header code into a printHeader fn  - improved structure of main by splitting out some of the code into a run() func

--- cmd/dumprom/main.go
+++ cmd/dumprom/main.go
@@ -9,10 +9,6 @@ import (
 	"log"
 	"os"
 	"strings"
-	"text/tabwriter"
-
-	"gitlab.com/aalbacetef/chipper"
-	"golang.org/x/exp/slices"
 )
 
 func main() {
@@ -28,156 +24,72 @@ func main() {
 
 	if !(dump || text) {
 		flag.Usage()
+
 		return
 	}
 
 	if name == "" {
 		flag.Usage()
+
 		return
 	}
 
 	data, err := os.ReadFile(name)
 	if err != nil {
 		log.Println("error reading file: ", err)
+
 		return
 	}
 
-	if dump {
-		fmt.Println("" +
-			"------------------\n" +
-			"|                |\n" +
-			"|      dump      |\n" +
-			"|                |\n" +
-			"------------------\n")
-		r := bytes.NewReader(data)
+	if err := run(data, dump, text); err != nil {
+		log.Println("error: ", err)
 
-		if err := dumpInstructions(r); err != nil && !errors.Is(err, io.EOF) {
-			fmt.Println("error: ", err)
-			return
-		}
-
-		fmt.Println("\n\n")
-	}
-
-	if text {
-		fmt.Println("" +
-			"------------------\n" +
-			"|                |\n" +
-			"|      text      |\n" +
-			"|                |\n" +
-			"------------------\n")
-		r := bytes.NewReader(data)
-
-		if err := runThrough(r); err != nil {
-			fmt.Println("error: ", err)
-			return
-		}
+		return
 	}
 }
 
-func dumpInstructions(r io.Reader) error {
-	p := make([]byte, 2)
+func run(data []byte, dump bool, text bool) error {
+	if dump {
+		fmt.Print(printHeader("dump"))
 
-	instructions := make(map[chipper.Opcode]int)
-	b := &strings.Builder{}
-	tw := tabwriter.NewWriter(
-		b, 0, 0, 1, ' ', tabwriter.TabIndent)
+		r := bytes.NewReader(data)
 
-	v := func() error {
-		for {
-			bytesRead, err := r.Read(p)
-			if errors.Is(err, io.EOF) {
-				return nil
-			}
+		if err := dumpInstructions(r); err != nil && !errors.Is(err, io.EOF) {
+			return err
+		}
 
-			if err != nil {
-				return fmt.Errorf("error reading: %w", err)
-			}
+		fmt.Print("\n\n")
+	}
 
-			if bytesRead == 0 {
-				return fmt.Errorf("read 0 bytes")
-			}
+	if text {
+		fmt.Print(printHeader("text"))
 
-			instr, err := chipper.Decode(p)
-			if err != nil {
-				continue
-			}
+		r := bytes.NewReader(data)
 
-			instructions[instr.Op]++
+		if err := toHumanReadable(r); err != nil {
+			return err
 		}
 	}
-
-	if err := v(); err != nil && !errors.Is(err, io.EOF) {
-		return fmt.Errorf("error ocurred: %w", err)
-	}
-
-	keys := make([]string, 0, len(instructions))
-	for key := range instructions {
-		keys = append(keys, string(key))
-	}
-
-	slices.Sort(keys)
-
-	for k, key := range keys {
-		fmt.Fprintf(tw, "%2d) %s \t(count: %3d)\n", k, key, instructions[chipper.Opcode(key)])
-	}
-
-	tw.Flush()
-	fmt.Println(b.String())
 
 	return nil
 }
 
-func runThrough(r io.Reader) error {
-	p := make([]byte, 2)
-	k := 0
-	b := &strings.Builder{}
-	tw := tabwriter.NewWriter(
-		b, 0, 0, 1, ' ', tabwriter.TabIndent)
+// NOTE: assumes text is 4 characters wide.
+func printHeader(text string) string {
+	const (
+		hrRow  = "------------------"
+		pad    = "|                |"
+		center = "|      %s      |"
+	)
 
-	defer func() {
-		tw.Flush()
-		fmt.Println(b.String())
-	}()
-
-	for {
-		bytesRead, err := r.Read(p)
-		if errors.Is(err, io.EOF) {
-			return nil
-		}
-
-		if err != nil {
-			return fmt.Errorf("error reading: %w", err)
-		}
-
-		if bytesRead == 0 {
-			return fmt.Errorf("read 0 bytes")
-		}
-
-		instr, err := chipper.Decode(p)
-		if err != nil {
-			fmt.Fprintf(
-				tw,
-				"%0#4x) could not decode \t=> %0#4x\n",
-				k,
-				uint16(p[0])<<8|uint16(p[1]),
-			)
-			continue
-		}
-
-		addr, err := chipper.ToAddr3(instr.Operands)
-		if err != nil {
-			return fmt.Errorf("could not parse addr3: %w", err)
-		}
-
-		fmt.Fprintf(
-			tw,
-			"%0#4x) %s \t=> %0#3x \t| %+v\n",
-			k+0x200, instr.Op,
-			addr,
-			instr.Operands,
-		)
-		k += bytesRead
-	}
-
+	return strings.Join(
+		[]string{
+			hrRow,
+			pad,
+			fmt.Sprintf(center, text),
+			pad,
+			hrRow,
+		},
+		"\n",
+	) + "\n"
 }
---
+++ cmd/dumprom/text.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"strings"
+	"text/tabwriter"
+
+	"gitlab.com/aalbacetef/chipper"
+)
+
+func toHumanReadable(r io.Reader) error {
+	const instructionWidth = 2
+	p := make([]byte, instructionWidth)
+
+	k := 0
+	b := &strings.Builder{}
+	tw := tabwriter.NewWriter(
+		b,
+		0, 0, 1, ' ',
+		tabwriter.TabIndent,
+	)
+
+	defer func() {
+		tw.Flush()
+		fmt.Println(b.String())
+	}()
+
+	for {
+		bytesRead, err := r.Read(p)
+		if errors.Is(err, io.EOF) {
+			return nil
+		}
+
+		if err != nil {
+			return fmt.Errorf("error reading: %w", err)
+		}
+
+		if bytesRead == 0 {
+			return fmt.Errorf("read 0 bytes")
+		}
+
+		instr, err := chipper.Decode(p)
+		if err != nil {
+			fmt.Fprintf(
+				tw,
+				"%0#4x) could not decode \t=> %0#4x\n",
+				k,
+				uint16(p[0])<<8|uint16(p[1]),
+			)
+
+			continue
+		}
+
+		addr, err := chipper.ToAddr3(instr.Operands)
+		if err != nil {
+			return fmt.Errorf("could not parse addr3: %w", err)
+		}
+
+		const offset = 0x200
+
+		fmt.Fprintf(
+			tw,
+			"%0#4x) %s \t=> %0#3x \t| %+v\n",
+			k+offset, instr.Op,
+			addr,
+			instr.Operands,
+		)
+
+		k += bytesRead
+	}
+}
