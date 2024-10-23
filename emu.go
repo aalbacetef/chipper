a747028d6292a1b8b2b305f30541ea6596cc5735
Arturo Albacete <aalbacetef@gmail.com>
2024-10-23 17:37:22 +0200

    reorganize main emu routines, add comments

--- emu.go
+++ emu.go
@@ -1,7 +1,6 @@
 package chipper
 
 import (
-	"context"
 	"errors"
 	"fmt"
 	"io"
@@ -27,7 +26,6 @@ type Emulator struct {
 	Stack           *Stack
 	RAM             []byte
 	Display         Display
-	cancelFns       []context.CancelFunc
 	LastInstruction Instruction
 	logger          *log.Logger
 	lastTick        time.Time
@@ -37,11 +35,7 @@ func (emu *Emulator) SetLogger(l *log.Logger) {
 	emu.logger = l
 }
 
-func (emu *Emulator) Close() {
-	for _, cancel := range emu.cancelFns {
-		cancel()
-	}
-}
+func (emu *Emulator) Close() { /* noop for now */ }
 
 func NewRAM(size int) ([]byte, error) {
 	if size <= 0 {
@@ -77,106 +71,7 @@ func NewEmulator(stackSize, ramSize int, d Display, keys KeyInputSource) (*Emula
 	return emu, nil
 }
 
-func (emu *Emulator) subtractTimers() {
-	period := time.Millisecond * 16
-	elapsed := time.Since(emu.lastTick)
-	if elapsed < period {
-		return
-	}
-
-	times := elapsed / period
-	sub := int(times)
-
-	if emu.DelayTimer > 0 {
-		dt := int(emu.DelayTimer) - sub
-		if dt < 0 {
-			dt = 0
-		}
-		emu.DelayTimer = byte(dt)
-	}
-
-	if emu.SoundTimer > 0 {
-		st := int(emu.SoundTimer) - sub
-		if st < 0 {
-			st = 0
-		}
-		emu.SoundTimer = byte(st)
-	}
-}
-
-// Tick .
-func (emu *Emulator) Tick() error {
-	if emu.lastTick.IsZero() {
-		emu.lastTick = time.Now()
-	}
-
-	emu.subtractTimers()
-
-	logger := emu.logger
-	if logger == nil {
-		logger = log.New(io.Discard, "[emu] ", log.Ltime)
-	}
-
-	logger.Println("fetch")
-	instrBytes, err := emu.Fetch(InstructionSize)
-	if errors.Is(err, io.EOF) {
-		return fmt.Errorf("reached last instruction: %w", io.EOF)
-	}
-
-	if err != nil {
-		log.Printf("error fetching: %v", err)
-		return fmt.Errorf("error fetching instruction: %w", err)
-	}
-
-	// update PC
-	emu.PC += uint16(InstructionSize)
-
-	logger.Println("decoding")
-
-	// decode
-	instr, err := Decode(instrBytes)
-	if err != nil {
-		log.Printf("error decoding: %v", err)
-		fmt.Println("could not decode : ", err)
-		return fmt.Errorf("could not decode instruction: %w", err)
-	}
-
-	emu.LastInstruction = instr
-
-	logger.Println("executing instruction: ", instr.String())
-
-	// execute
-	execErr := emu.Execute(instr)
-	if execErr != nil {
-		logger.Printf("error executing: %v", err)
-		fmt.Println("could not execute: ", err)
-		return execErr
-	}
-
-	return nil
-}
-
-// fetch .
-func (emu *Emulator) Fetch(numBytes int) ([]byte, error) {
-	pc := int(emu.PC)
-	ramSize := len(emu.RAM)
-
-	if pc+numBytes >= ramSize {
-		return nil, fmt.Errorf(
-			"out of bounds (PC=%d, numBytes=%d, RAMSize=%d): %w",
-			pc, numBytes, ramSize, io.EOF,
-		)
-	}
-
-	read := make([]byte, numBytes)
-	for k := 0; k < numBytes; k++ {
-		read[k] = emu.RAM[pc+k]
-	}
-
-	return read, nil
-}
-
-// Load .
+// Load will read the ROM from the passed in io.Reader and load the sprites.
 func (emu *Emulator) Load(r io.Reader) error {
 	if err := loadSprites(emu); err != nil {
 		return fmt.Errorf("could not load sprites: %w", err)
@@ -198,4 +93,107 @@ func (emu *Emulator) Load(r io.Reader) error {
 	}
 
 	return nil
+}
+
+// subtractTimers is our routine for reducing the timers. It is meant to run at 60Hz (~16ms).
+func (emu *Emulator) subtractTimers() {
+	const timerPeriod = 16 * time.Millisecond
+
+	elapsed := time.Since(emu.lastTick)
+	if elapsed < timerPeriod {
+		return
+	}
+
+	times := elapsed / timerPeriod
+	sub := int(times)
+
+	if emu.DelayTimer > 0 {
+		dt := int(emu.DelayTimer) - sub
+		if dt < 0 {
+			dt = 0
+		}
+
+		emu.DelayTimer = byte(dt)
+	}
+
+	if emu.SoundTimer > 0 {
+		st := int(emu.SoundTimer) - sub
+		if st < 0 {
+			st = 0
+		}
+
+		emu.SoundTimer = byte(st)
+	}
+}
+
+// Tick is the core Fetch-Decode-Execute loop of the emulator.
+func (emu *Emulator) Tick() error {
+	// set the field
+	emu.lastTick = time.Now()
+
+	emu.subtractTimers()
+
+	logger := emu.logger
+	if logger == nil {
+		logger = log.New(io.Discard, "[emu] ", log.Ltime)
+	}
+
+	logger.Println("fetch")
+
+	instrBytes, err := emu.Fetch(InstructionSize)
+	if errors.Is(err, io.EOF) {
+		return fmt.Errorf("reached last instruction: %w", io.EOF)
+	}
+
+	if err != nil {
+		return fmt.Errorf("error fetching instruction: %w", err)
+	}
+
+	// update PC
+	emu.PC += uint16(InstructionSize)
+
+	logger.Println("decoding")
+
+	// decode
+	instr, err := Decode(instrBytes)
+	if err != nil {
+		log.Printf("error decoding: %v", err)
+		fmt.Println("could not decode : ", err)
+		return fmt.Errorf("could not decode instruction: %w", err)
+	}
+
+	// store the last instruction, useful for debugging
+	emu.LastInstruction = instr
+
+	logger.Println("executing instruction: ", instr.String())
+
+	// execute
+	execErr := emu.Execute(instr)
+	if execErr != nil {
+		logger.Printf("error executing: %v", err)
+		fmt.Println("could not execute: ", err)
+		return execErr
+	}
+
+	return nil
+}
+
+// Fetch will read the instruction pointed at by the PC. It will do a bounds check.
+func (emu *Emulator) Fetch(numBytes int) ([]byte, error) {
+	pc := int(emu.PC)
+	ramSize := len(emu.RAM)
+
+	if pc+numBytes >= ramSize {
+		return nil, fmt.Errorf(
+			"out of bounds (PC=%d, numBytes=%d, RAMSize=%d): %w",
+			pc, numBytes, ramSize, io.EOF,
+		)
+	}
+
+	read := make([]byte, numBytes)
+	for k := 0; k < numBytes; k++ {
+		read[k] = emu.RAM[pc+k]
+	}
+
+	return read, nil
 }
