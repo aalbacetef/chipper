12b478d3b7be4cbfe602b045fcb5f745ea6ae295
Arturo Albacete <aalbacetef@gmail.com>
2024-10-25 18:02:55 +0200

    add a few cases to the ToByte test

--- util_test.go
+++ util_test.go
@@ -16,14 +16,28 @@ func TestUtil(t *testing.T) {
 	})
 
 	t.Run("ToByte", func(t *testing.T) {
-		got, err := ToByte([]int{0x2, 0x3})
-		if err != nil {
-			t.Fatalf("got error: %v", err)
+		cases := []struct {
+			label string
+			args  []int
+			want  byte
+		}{
+			{"0x23", []int{0x2, 0x3}, 0x23},
+			{"0x11", []int{0x1, 0x1}, 0x11},
 		}
 
-		const want = 0x23
-		if got != want {
-			t.Fatalf("want #%0x, got #%0x", want, got)
+		for _, c := range cases {
+			t.Run(c.label, func(t *testing.T) {
+				got, err := ToByte(c.args)
+				if err != nil {
+					t.Fatalf("got error: %v", err)
+				}
+
+				if got != c.want {
+					t.Fatalf("want #%0x, got #%0x", c.want, got)
+				}
+
+			})
 		}
+
 	})
 }
