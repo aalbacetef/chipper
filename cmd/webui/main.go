16e96a402dcd677881cc1f297ba340ac32eb8465
Arturo Albacete <aalbacetef@gmail.com>
2024-10-28 09:35:08 +0100

    add support for setting the tick period from the UI; return 0 for success from wasm functions; 1 for errors

--- cmd/webui/main.go
+++ cmd/webui/main.go
@@ -14,13 +14,17 @@ import (
 
 func main() {
 	const (
-		stackSize  = 16
-		delay      = 16
-		RAMSize    = 4*1024 + 1
-		w          = 64
-		h          = 32
-		tickPeriod = 2 * time.Millisecond
+		stackSize         = 16
+		delay             = 16
+		RAMSize           = 4*1024 + 1
+		w                 = 64
+		h                 = 32
+		defaultTickPeriod = 2 * time.Millisecond
 	)
+
+	// NOTE: When the UI changes this, it should first STOP the emulator, update
+	// the tickPeriod, then Start the emulator.
+	tickPeriod := defaultTickPeriod
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -65,7 +69,7 @@ func main() {
 
 		if n != wantLen {
 			fmt.Printf("want %d args, got %d", wantLen, n)
-			return nil
+			return 1
 		}
 
 		key := args[0].Int()
@@ -76,22 +80,38 @@ func main() {
 
 		wrapper.keySrc.Set(key, v)
 
-		return nil
+		return 0
 	})
 
 	startFn := js.FuncOf(func(this js.Value, args []js.Value) any {
 		wrapper.start(ctx, tickPeriod)
-		return nil
+		return 0
 	})
 
 	stopFn := js.FuncOf(func(this js.Value, args []js.Value) any {
 		wrapper.stop()
-		return nil
+		return 0
 	})
 
 	restartFn := js.FuncOf(func(this js.Value, args []js.Value) any {
 		wrapper.restart()
-		return nil
+		return 0
+	})
+
+	tickerPeriodFn := js.FuncOf(func(this js.Value, args []js.Value) any {
+		m, n := 1, len(args)
+		if n != m {
+			fmt.Printf("expected args to have %d elements, got %d\n", m, n)
+			return 1
+		}
+
+		period := time.Millisecond * time.Duration(args[0].Int())
+
+		wrapper.stop()
+		tickPeriod = period
+		wrapper.start(ctx, tickPeriod)
+
+		return 0
 	})
 
 	js.Global().Set("RestartEmu", restartFn)
@@ -100,6 +120,7 @@ func main() {
 	js.Global().Set("LoadROM", loadROMFn)
 	js.Global().Set("GetDisplay", sendDisplayToWASM)
 	js.Global().Set("SendKeyboardEvent", handleKeyPress)
+	js.Global().Set("SetTickerPeriod", tickerPeriodFn)
 
 	select {}
 }
